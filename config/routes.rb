e0d097411a27e11942d3e137cc3d66df79b04aab
Lisa Chinn <lisamchinn@gmail.com>
2019-07-09 16:35:58 -0300

    finish rails wikipedia

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
 end
+
+  # read all
+  # get 'articles', to: 'articles#index'
+  # # read one
+  # get 'articles/:id', to: 'articles#show'
+  # # create new (2 requests)
+  # get 'articles/new', to: 'articles#new'
+  # post 'articles', to: 'articles#create'
+  # # update (2 requests)
+  # get 'articles/:id/edit', to: 'articles#edit'
+  # patch 'articles/:id', to: 'articles#update'
+  # # delete
+  # delete 'articles/:id', to: 'articles#destroy'
